a5873a5e43e93d711863fa489e9698cf1eadb4a9
farsink <farsin495@gmail.com>
2025-07-11 17:31:32 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    allowedHosts : ['all', '.ngrok-free.app', '8d3cef6c4ef4.ngrok-free.app']
+  }
+})
